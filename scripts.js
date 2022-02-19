949fae661cd36aae4a10e221ea3c3f3cf828a5e4
laiberry <iibriaan@gmail.com>
2022-02-18 20:24:36 -0800

    start with create-react-site

--- scripts.js
+++
@@ -1,6 +0,0 @@
-let nav = document.querySelector('nav');
-let menu = document.querySelector('.icon-menu')
-
-menu.addEventListener('click', function() {
-	nav.classList.toggle('open');
-});
---
+++ src/App.js
@@ -0,0 +1,13 @@
+/* import stylesheet */
+import './styles.css';
+
+/* import components */
+import Header from './components/Header';
+
+function App() {
+	return (
+		<h1>Hellodsfsdfss Wrold</h1>
+	);
+}
+
+export default App;
