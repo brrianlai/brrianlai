3adfe1fdd8da65f38bd60621e201f7133d1ab950
brrianlai <iibriaan@gmail.com>
2022-09-24 15:35:59 -0700

    Optimized stylesheet delivery

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 const Document = () => {
 	return (
-		<Html lang='en'>
+		<Html lang="en">
 			<Head>
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 				<link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap' />
+				<link rel="preconnect" href="https://cdn.jsdelivr.net/" />
+				<link rel="preconnect" href="https://cdn.jsdelivr.net/" crossorigin />
 				<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css' />
 			</Head>
 			<body>
