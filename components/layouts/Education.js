eb08bcc0a0dfab061f82b50891c8a4fdb0a3e892
brrianlai <iibriaan@gmail.com>
2022-10-11 16:53:24 -0700

    Updates

--- components/layouts/Education.js
+++ components/layouts/Education.js
@@ -9,7 +9,7 @@ const Education = () => {
 				<h5>BSc in Biology</h5>
 				<p>Apr 2022</p>
 				<p>Canada</p>
-				<p>Awarded the B.C. Achievement scholarship in 2018</p>
+				<p>Awarded the B.C. Achievement Scholarship in 2018</p>
 			</div>
 		</section>
 	)
