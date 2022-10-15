9e1bb797537602e698da9c19eb6631a37c58ed98
brrianlai <iibriaan@gmail.com>
2022-10-15 11:09:45 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,6 @@ const Document = () => {
 		<Html lang="en">
 			<Head>
 				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" />
-				<link rel="preconnect" href="https://cdn.jsdelivr.net/" />
-				<link rel="preconnect" href="https://cdn.jsdelivr.net/" crossorigin />
-				<link rel="preload" href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" as="style" />
 				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" />
 			</Head>
 			<body>
