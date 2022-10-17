39b5f84555b5775e5234c34948868b26d40b2ffd
brrianlai <iibriaan@gmail.com>
2022-10-16 23:46:03 -0700

    Updates

--- components/layouts/Footer.js
+++ components/layouts/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import dynamic from 'next/dynamic'
-const ScrollToTop = dynamic(() => import('../ScrollToTop'))
+import ScrollToTop from '../ScrollToTop'
 
 const Footer = () => {
 	return (
